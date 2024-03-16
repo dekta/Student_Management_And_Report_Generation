a4cd0cec5f2124d2b2a90612d1ae5602bc006c79
EktaDhawal <51295793+dekta@users.noreply.github.com>
2024-03-16 23:51:39 +0530

    readmeFile updated and swagger updated

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Student_Management_And_Report_Generation
+
+## Introduction
+The purpose of this project is to showcase proficiency in implementing CRUD (Create, Read, Update, Delete) operations and designing relevant endpoints for a MongoDB-backed system. The system involves managing students, subjects, and marks, with the additional requirement of generating comprehensive report cards.
+
+## Contents
+- [Installation & Getting started](#installation--getting-started)
+- [Deployed](#deployed-app)
+- [Built With](#built-with)
+- [APIs Documentation](#apis-documentation)
+
+
+
+## Installation & Getting started
+- `git clone` the repo to your local machine. 
+- To use this application, please run the following command:
+    - `npm install`
+    - `cd Student_Management_And_Report_Generation`
+- Type the following command in your termimal to run the server:
+    - `npm start`
+
+
+## Deployed App
+- Backend: 'https://red-funny-chameleon.cyclic.app'
+- Platform = cyclic
+
+## Built With
+* Javascript
+* NodeJS
+* ExpressJS
+* MongoDB
+
+
+## APIs Documentation
+
+### Base API
+- **Local** = `http://localhost:3000`
+- **Deployed** = `https://red-funny-chameleon.cyclic.app`
+- **Swagger** =  `https://red-funny-chameleon.cyclic.app/api-docs/`
+
+
--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -8,7 +8,7 @@ const swaggerDefinition = {
   },
   components: {
     schemas: {
-      Auth: {
+      Users: {
         type: 'object',
         required: ['name', 'email', 'password'],
         properties: {
@@ -29,6 +29,86 @@ const swaggerDefinition = {
           name: 'John Doe',
           email: 'john@gmail.com',
           password: 'john1234'
+        }
+      },
+      Subjects: {
+        type: 'object',
+        required: ['name', 'gradeLevel'],
+        properties: {
+          name: {
+            type: 'string',
+            description: 'Name of subject'
+          },
+          gradeLevel: {
+            type: 'number',
+            description: 'Grade Level'
+          }
+        },
+        example: {
+          name: 'maths',
+          gradeLevel: 6
+        }
+      },
+      Students: {
+        type: 'object',
+        required: ['name', 'motherName', 'age', 'grade'],
+        properties: {
+          name: {
+            type: 'string',
+            description: 'Name of student'
+          },
+          motherName: {
+            type: 'string',
+            description: 'Mother name'
+          },
+          age: {
+            type: 'number',
+            description: '12'
+          },
+          grade: {
+            type: 'number',
+            description: '6'
+          }
+        },
+        example: {
+          name: 'john',
+          motherName: 'abc',
+          age: 12,
+          grade: 6
+        }
+      },
+      Marks: {
+        type: 'object',
+        required: ['studentId', 'grade', 'subjects'],
+        properties: {
+          studentId: {
+            type: 'string',
+            description: 'Student Id'
+          },
+          grade: {
+            type: 'number',
+            description: '6'
+          },
+          subjects: {
+            type: '[]',
+            description: []
+          }
+        },
+        example: {
+          studentId: '123456789',
+          grade: '6',
+          subjects: [
+            {
+              subject: 'english',
+              score: 60,
+              _id: '65f5e55056f07d574829ca5e'
+            },
+            {
+              subject: 'maths',
+              score: 55,
+              _id: '65f5e55056f07d574829ca5f'
+            }
+          ]
         }
       }
     }
